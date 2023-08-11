7ef100068ee9e35d31ed4b90ea6471527fae4b19
Aleksandra Olejarz <oluta525@gmail.com>
2023-08-11 02:23:16 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿// switch case
+Console.WriteLine("What's your favourite day of the number (Enter a number from 1 to 10)?");
+int favNumber = Convert.ToInt16(Console.ReadLine());
+switch(favNumber)
+{
+    case 1: 
+        Console.WriteLine("Your favourite number is 1, is Monday your favourire day of the week and January your favourite month as well?");
+        break;
+    case 2:
+        Console.WriteLine("Your favourite number is 2, is Tueasday your favourite day of the week and February your favourite month?");
+        break;
+    case 3:
+        Console.WriteLine("Your favourite number is 3, is Wednesday your favourite day of the week and March your favourite month as well?");
+        break;
+
+}
